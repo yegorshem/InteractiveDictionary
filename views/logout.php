25484e304c8f3e39b53ad8e1034d520c3d7be2b9
JoelMWatson <jwatson10@mail.greenriver.edu>
2016-10-08 17:03:53 -0700

    Created a login modal and a navbarView. Also made a logout option on the navbar

---
+++ views/logout.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joel
+ * Date: 10/8/2016
+ * Time: 3:05 PM
+ */
+
+session_start();
+
+session_unset();
+session_destroy();
+header("Location: ../controllers/adminController.php");
+?>
