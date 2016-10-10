c4bd774e84f4cf8480e5727d76f5081c196ad1fe
JoelMWatson <jwatson10@mail.greenriver.edu>
2016-10-10 12:37:19 -0700

    created a simple student page

---
+++ views/studentView.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yegor Shemereko
+ * Date: 10/7/2016
+ * Time: 10:42 AM
+ */
+
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Interactive Dictionary</title>
+    <meta name="description" content="Phonetic and visual dictionary for I-BEST Students">
+    <meta name="author" content="Team J.J.A.Y.">
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
+</head>
+<body>
+<?php include "navbarView.php"; ?>
+<h1>Hello, world!</h1>
+
+
+<?php include 'modals/login.php'; ?>
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
