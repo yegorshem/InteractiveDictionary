9da67d4c8a4a3e5dfec0a7671a94d37f8c3f75a7
JoelMWatson <jwatson10@mail.greenriver.edu>
2016-10-31 20:53:59 -0700

    adjusting merge conflicts

--- views/aboutView.php
+++ views/aboutView.php
@@ -13,26 +13,34 @@
 <head>
     <meta charset="utf-8">
 
-    <title>Interactive Dictionary</title>
+    <title>Carpentry English</title>
+
     <meta name="description" content="Phonetic and visual dictionary for I-BEST Students">
     <meta name="author" content="Team J.J.A.Y.">
 
     <!-- Bootstrap -->
     <link href="../css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="../css/form_formatter.css" rel="stylesheet" media="screen">
+    <link href="../css/background.css" rel="stylesheet" media="screen">
+    <link href="../css/footer.css" rel="stylesheet">
 
 </head>
 <body>
     <?php include "navbarView.php"; ?>
-    <h1>Interactive Dictionary</h1>
+    <h1>Carpentry English</h1>
 
-    <img src="../imgs/underConstruction.png" />
+    <div id="container">
 
+
+
+
+    </div>
 
     <?php include 'modals/login.php'; ?>
     <?php include 'modals/register.php'; ?>
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/login.js"></script>
+    <script src="../js/register.js"></script>
+
 </body>
 </html>
