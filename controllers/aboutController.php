793230ae6e6a37f3fae8e50138dd633a15075f7c
JoelMWatson <jwatson10@mail.greenriver.edu>
2016-10-31 20:21:04 -0700

    Removed unnecessary for validation

--- controllers/aboutController.php
+++ controllers/aboutController.php
@@ -8,45 +8,6 @@
 session_start();
 $thisPage = 'About';
 
-//validation
-$isValid = true;
-
-if (isset($_POST['username']) && $_POST['username'] != "") {
-    $username = $_POST['username'];
-} else {
-    $isValid = false;
-}
-if (isset($_POST['password']) && $_POST['password'] != "") {
-    $password = $_POST['password'];
-} else {
-    $isValid = false;
-}
-
-if ($isValid) {
-    //Includes DB files
-    $config = include("../config.php");
-
-    try {
-        $db = new PDO ($config["connectionString"], $config["username"], $config["password"]);
-
-        //set the PDO error mode to exception
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Connected successfully";
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-
-    require '../models/UserAdapter.php';
-
-    $adapter = new UserAdapter($db);
-
-    $user = $adapter->loginFunction($username, $password);
-    echo $user->getPriority();
-    if ($user != null) {
-        $_SESSION['user'] = $user->getPriority();
-    }
-
-}
 
 require '../views/aboutView.php';
 ?>
