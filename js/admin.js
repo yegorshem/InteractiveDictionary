7e8f0151788c8ca516bc573a1f92f2121e8ee951
yegorshem <yshemereko@mail.greenriver.edu>
2016-11-02 09:19:00 -0700

    Changed accent of responsiveVoice && changed glyphicon to a volume-up button

--- js/admin.js
+++ js/admin.js
@@ -5,7 +5,6 @@
 var $table = $('#adminTable');
 
 
-
 window.operateEvents = {
     'click .edit': function (e, value, row, index) {
 
@@ -19,7 +18,7 @@ window.operateEvents = {
     },
 
     'click .voice': function (e, value, row, index) {
-        responsiveVoice.speak(row.word);
+        responsiveVoice.speak(row.word, "US English Female");
     }
 };
 
@@ -77,8 +76,8 @@ function operateFormatter(value, row, index) {
  */
 function imageFormatter(value, row, index) {
     return [
-        '<a class="image" href="../uploads/'+value+'" title="Image">',
-        '<img height="100" src="../uploads/'+value+'">',
+        '<a class="image" href="../uploads/' + value + '" title="Image">',
+        '<img height="100" src="../uploads/' + value + '">',
         '</a>'
     ].join('');
 }
@@ -87,7 +86,7 @@ function imageFormatter(value, row, index) {
 function voiceFormatter(value, row, index) {
     return [
         '<a class="voice" href="javascript:void(0)" title="Voice">',
-        '<i class="glyphicon glyphicon-bell"></i>',
+        '<i class="glyphicon glyphicon-volume-up"></i>',
         '</a>'
     ].join('');
 }
@@ -139,8 +138,6 @@ $table.bootstrapTable({
         //...
     }
 });
-
-
 
 
 /**
@@ -239,7 +236,7 @@ $(function () {
                 });
             });
         }
-    }
+    };
 
     // Update word --------------------------------------------------
     $("#update-word-form").on('submit', function (e) {
